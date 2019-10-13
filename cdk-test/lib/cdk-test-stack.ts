5f83d7291aa676dcf0321c81ef9991531bd12a0a
ishizukayusuke <ishizukayusuke@team-lab.com>
2019-10-13 23:19:17 +0900

    スタック定義

--- cdk-test/lib/cdk-test-stack.ts
+++ cdk-test/lib/cdk-test-stack.ts
@@ -1,9 +1,18 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2')
+import { Bucket } from "@aws-cdk/aws-s3"
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "CdkTest", {
+      maxAzs: 3
+    });
+
+    // Create S3 Bucket
+    new Bucket(this, id, {
+      bucketName: "ishizuka-bucket",
+    })
   }
 }
